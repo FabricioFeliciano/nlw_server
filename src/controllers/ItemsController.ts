decf366f5a7c6cc653b813419d146260ebd1f8e7
Fabricio Feliciano <fabriciofeliciano@hotmail.com>
2020-06-06 22:42:17 -0300

    Correcao da porta outra vez

--- src/controllers/ItemsController.ts
+++ src/controllers/ItemsController.ts
@@ -11,7 +11,7 @@ class ItemsController {
             return {
                 id: item.id,
                 title: item.title,
-                image_url: `${process.env.REACT_APP_URL}/uploads/${item.image}`
+                image_url: `${process.env.URL}/uploads/${item.image}`
             };
         })
 
